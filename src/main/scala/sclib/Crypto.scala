5f6c84b5734c1aea9e0aac4f968b5e267d1f89b3
Ikumi Shimizu <193s@users.noreply.github.com>
2014-11-22 20:57:39 +0900

    update README.md | rename

--- src/main/scala/sclib/Crypto.scala
+++ src/main/scala/sclib/Crypto.scala
@@ -5,5 +5,6 @@ import java.util.Base64
 
 object Crypto {
   def base64(str: String) = new String(Base64.getEncoder.encode(str.getBytes))
+  def base64noPadding(str: String) = new String(Base64.getEncoder.withoutPadding.encode(str.getBytes))
   def base64decode(str: String) = new String(Base64.getDecoder.decode(str))
 }
