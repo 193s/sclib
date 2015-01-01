99dbe116c0982734ae3878f10809f07ca2db12ca
Ikumi Shimizu <193s@users.noreply.github.com>
2015-01-01 23:04:52 +0900

    update #10 add methods: setAuth, readAll

--- src/main/scala/sclib/Web.scala
+++ src/main/scala/sclib/Web.scala
@@ -1,9 +1,10 @@
 package sclib
 
+import java.io.InputStream
 import java.net.URLEncoder
-
+import java.net.Authenticator
+import java.net.PasswordAuthentication
 import sun.net.idn.Punycode
-
 import scala.io.Source
 
 
@@ -12,4 +13,13 @@ object Web {
   def urlencode(str: String) = URLEncoder.encode(str, "UTF-8")
   def punycode(str: String) = Punycode.encode(new StringBuffer(str), null).toString
   def punycodeDecode(str: String) = Punycode.decode(new StringBuffer(str), null).toString
+  def readAll(in: InputStream) =
+    Stream.continually(in.read()).takeWhile(-1 !=).map(_.toChar).mkString
+
+  def setAuth(usr: String, passwd: String): Unit = Authenticator.setDefault (
+    new Authenticator() {
+      override protected def getPasswordAuthentication() =
+        new PasswordAuthentication(usr, passwd.toCharArray)
+    }
+  )
 }
